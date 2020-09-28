8fa436609881212bb0c62fc4dd1a2e363f93d1d3
Hieu Trinh <hieu.d.trinh@gmail.com>
2020-09-28 12:43:35 -0700

    hackerrank - warm up - max sum of the hour glass in 2D arrays

---
+++ src/main/java/arrays/max_hour_glass_sum/MaximumHourGlassSumInA2D.java
@@ -0,0 +1,25 @@
+package arrays.max_hour_glass_sum;
+
+public class MaximumHourGlassSumInA2D {
+  private int hcsum(int[][] a, int i, int j) {
+    return a[i][j]
+        + a[i][j + 1]
+        + a[i][j + 2]
+        + a[i + 1][j + 1]
+        + a[i + 2][j]
+        + a[i + 2][j + 1]
+        + a[i + 2][j + 2];
+  }
+
+  public int hourglassSum(int[][] arr) {
+    // there are 7 cells in the hour glass, so min can be 7*(-9)
+    int max = -99;
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        int sum = hcsum(arr, i, j);
+        max = Math.max(max, sum);
+      }
+    }
+    return max;
+  }
+}
