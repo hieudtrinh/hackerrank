4bd60af5cb66fa8fd19962095748c9ea4c1175e8
Hieu Trinh <hieu.d.trinh@gmail.com>
2020-09-28 18:00:56 -0700

    hackerrank - array - minimum swap two - sort an array

--- src/test/java/arrays/minimum_swaps_two/MinimumSwapsTwoTest.java
+++ src/test/java/arrays/minimum_swaps_two/MinimumSwapsTwoTest.java
@@ -9,8 +9,13 @@ public class MinimumSwapsTwoTest {
   @Test
   public void testMinimumSwaps() {
     int[] arr = {7, 1, 3, 2, 4, 5, 6};
+    int[] arr2 = {2, 3 ,4 ,1, 5};
+
     int ans = 5;
+    int ans2 = 3;
     MinimumSwapsTwo solution = new MinimumSwapsTwo();
     assertEquals(solution.minimumSwaps(arr), ans);
+    assertEquals(solution.minimumSwaps(arr2), ans2);
+
   }
 }
